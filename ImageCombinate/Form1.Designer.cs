dbd811f977733c1f15e2855a002216526894019b
z-bruno <yszhang2018@qq.com>
2019-04-18 16:27:23 +0800

    1.拼接三张图片 2.保留最大高度，高度不足的图片锁定横纵比线性拉长 3.可另存为

---
+++ ImageCombinate/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ImageCombinate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.OpenFileDialog_btn = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Stitch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(329, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(635, 42);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(23, 276);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(900, 200);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // OpenFileDialog_btn
+            // 
+            this.OpenFileDialog_btn.Location = new System.Drawing.Point(54, 13);
+            this.OpenFileDialog_btn.Name = "OpenFileDialog_btn";
+            this.OpenFileDialog_btn.Size = new System.Drawing.Size(94, 23);
+            this.OpenFileDialog_btn.TabIndex = 5;
+            this.OpenFileDialog_btn.Text = "OpenPictures";
+            this.OpenFileDialog_btn.UseVisualStyleBackColor = true;
+            this.OpenFileDialog_btn.Click += new System.EventHandler(this.OpenFileDialog_btn_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(248, 248);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 6;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 565);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.OpenFileDialog_btn);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "图片拼接";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button OpenFileDialog_btn;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ ImageCombinate/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ImageCombinate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image img1 = pictureBox1.Image;
+            Image img2 = pictureBox2.Image;
+            Image img3 = pictureBox3.Image;
+
+            int width = img1.Width + img2.Width + img3.Width;
+            int h = Math.Max(img1.Height, img2.Height);
+            int height = Math.Max(h, img3.Height);
+            Bitmap bitmap = new Bitmap(width, height);
+            //Graphics g = this.CreateGraphics();  //在界面创建画布
+            Graphics g = Graphics.FromImage(bitmap);    //以bitmap为基础创建画布
+            //g.DrawImage(img1, 0, 0, img1.Width, img1.Height);
+            //g.DrawImage(img2, img1.Width, 0, img2.Width, img2.Height);
+            //g.DrawImage(img3, img1.Width + img2.Width, 0, img3.Width, img3.Height);
+
+            int w1 = ((img1.Width / img1.Height) * height);
+            int w2 = ((img2.Width / img2.Height) * height);
+            int w3 = ((img3.Width / img3.Height) * height);
+
+            g.DrawImage(img1, 0, 0, w1, height);
+            g.DrawImage(img2, w1, 0, w2, height);
+            g.DrawImage(img3, w1+w2, 0, w3, height);
+            Image img = bitmap;
+            pictureBox4.Image = img;
+
+        }
+
+        private void OpenFileDialog_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "图片文件（*.bmp,*.jpg,*.png)|*.bmp;*.jpg;*.png|所有文件（*.*）|*.*";
+            openFileDialog1.Title = "选择图片";
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.ShowDialog();
+            Image<Bgr, byte>[] sources = new Image<Bgr, byte>[openFileDialog1.FileNames.Length];
+            for (int i = 0; i < openFileDialog1.FileNames.Length; i++)
+            {
+                sources[i] = new Image<Bgr, byte>(openFileDialog1.FileNames[i]);
+            }
+            if (openFileDialog1.FileNames.Length ==3 )
+            {
+                pictureBox1.Image = sources[0].Bitmap;
+                pictureBox2.Image = sources[1].Bitmap;
+                pictureBox3.Image = sources[2].Bitmap;
+            }
+            else MessageBox.Show("请选取至少但不超过3张图片！");
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SavePic = new SaveFileDialog();
+            SavePic.Title = "图片另存为";
+            SavePic.Filter = "图片文件（*.bmp,*.jpg,*.png)|*.bmp;*.jpg;*.png|所有文件（*.*）|*.*";
+            SavePic.ShowDialog();
+            pictureBox4.Image.Save(SavePic.FileName);  //FileName实际是ShowDialog界面生成的文件的完整路径
+        }
+    }
+}
